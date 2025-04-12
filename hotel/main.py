514c8a0abbe89694a1719efecc5b438e2eee6e46
bostroja <bostroja@arcada.fi>
2025-04-12 10:27:33 +0000

    CatchUp2

--- hotel/main.py
+++ hotel/main.py
@@ -2,15 +2,21 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
-PORT=8062
+PORT=8065
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-@app.get("/")
-def getIp(request: Request):
-    ip = request.client.host
-    return {'ip': ip}
+# "List of dicts" är ungefär samma som en "array of objects" (i JS)
+rooms = [
+    {"number": 303, "type": "single", "price": 150},
+    {"number": 404, "type": "double", "price": 200},
+    {"number": 505, "type": "suite", "price": 249.99}
+]
+
+@app.get("/rooms")
+def getRooms(request: Request):
+   return rooms
 
 if __name__ == "__main__":
     uvicorn.run(
